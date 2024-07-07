28ef4b1a15bc62965c598c806c8f91c952b7f0d6
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-07-06 18:43:31 -0700

    Finished the button(s) functionality.

--- script.js
+++ script.js
@@ -6,23 +6,32 @@ function markerFactory(type, symbolText, color)
     return marker;
 }
 
-function checkForWin()
-{
-    // if()
-    // {
-    //     // Increment either player one or player two score here.
-    // }
-}
-
 const playerOneMarker = markerFactory("div", "X", "#FF6962");
 const playerTwoMarker = markerFactory("div", "O", "#AFDCED");
-console.log(playerOneMarker.textContent)
-
+const playerOneScore_label = document.querySelector(".player_one_score");
+const playerTwoScore_label = document.querySelector(".player_two_score");
 let currentPlayer = 1;
+let playerOneScore = 0, playerTwoScore = 0;
 
 const areas = document.querySelectorAll(".board div");
 const reset_all = document.querySelector("button.reset-all");
 const reset_game = document.querySelector("button.reset-game");
+
+function checkForWin()
+{
+    if(areas[0].textContent === "O" && areas[0].textContent === areas[1].textContent && areas[1].textContent === areas[2].textContent)
+    {
+        playerTwoScore++;
+        playerTwoScore_label.textContent = playerTwoScore;
+        console.log("Printing it out now.");
+    }
+    if(areas[3].textContent === "O" && areas[3].textContent === areas[4].textContent && areas[4].textContent === areas[5].textContent)
+    {
+        playerTwoScore++;
+        playerTwoScore_label.textContent = playerTwoScore;
+        console.log("Printing it out now.");
+    }
+}
 
 for(let i = 0; i < areas.length; i++)
 {
@@ -32,16 +41,18 @@ for(let i = 0; i < areas.length; i++)
         {
             areas[i].style.backgroundColor = playerOneMarker.style.color;
             areas[i].textContent = playerOneMarker.textContent;
+            console.log(areas[i].textContent);
             currentPlayer = 2;
         }
         else
         {
             areas[i].style.backgroundColor = playerTwoMarker.style.color;
             areas[i].textContent = playerTwoMarker.textContent;
+            console.log(areas[i].textContent);
             currentPlayer = 1;
         }
+        checkForWin();
     });
-    checkForWin();
 }
 
 // Only difference with reset all button is that the scores for both players will be reset to 0.
@@ -53,4 +64,19 @@ reset_game.addEventListener("click", () =>
         areas[i].style.backgroundColor = "tan";
     }
     currentPlayer = 1;
+});
+
+reset_all.addEventListener("click", () =>
+{
+
+    for(let i = 0; i < areas.length; i++)
+    {
+        areas[i].textContent = "";
+        areas[i].style.backgroundColor = "tan";
+    }
+    currentPlayer = 1;
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    playerOneScore_label.textContent = playerOneScore;
+    playerTwoScore_label.textContent = playerTwoScore;
 });
